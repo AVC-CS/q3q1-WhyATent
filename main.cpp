853e4bb839cfd9b7cad0a78756a0dc84e5803677
WhyATent <WWyatnt@gmail.com>
2026-03-13 23:14:04 -0700

    All the values that are supposed to be inputted by the pytest give me the correct alues when tested with ./a.out. Unsure why it is not working in the autograder.

--- main.cpp
+++ main.cpp
@@ -19,9 +19,9 @@ int main()
 
     cout << setprecision(2) << fixed;
     if (firstroot > secondroot)
-        cout << firstroot << "." << secondroot << endl;
+        cout << "roots " << firstroot << "." << secondroot << endl;
     else 
-        cout << secondroot << "." << firstroot << endl;
+        cout << "roots " << secondroot << " " << firstroot << endl;
 
     return 0;
 }
