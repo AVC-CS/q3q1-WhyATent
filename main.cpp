95f1269df3977c922be15999ab4d2cf7ce93358f
WhyATent <WWyatnt@gmail.com>
2026-03-13 23:30:27 -0700

    Moving variables, perhaps that will make a difference in the autograder.

--- main.cpp
+++ main.cpp
@@ -6,11 +6,10 @@ using namespace std;
 int main()
 {
     double a, b, c;
-    // TODO
+    double firstpart, positive, negative, firstroot, secondroot;
     cout << "Input three (double) values in a line separated by spaces (e.g., a b c):\n";
     cin >> a >> b >> c;
     // The quadratic equation was provided in question 1 of the quiz. Since it will be difficult to do the whole thing in one go (for me), I will separate the various parts. That was, when I do the +-, I can create two different square roots. 
-   double firstpart, positive, negative, firstroot, secondroot;
     firstpart = b * b - (4 * a * c);
     positive = -b + sqrt(firstpart);
     negative = -b - sqrt(firstpart);
@@ -19,7 +18,7 @@ int main()
 
     cout << setprecision(2) << fixed;
     if (firstroot > secondroot)
-        cout << "roots " << firstroot << "." << secondroot << endl;
+        cout << "roots " << firstroot << " " << secondroot << endl;
     else 
         cout << "roots " << secondroot << " " << firstroot << endl;
 
